0ad87384ddabe1c88fac124655c4804f7350c6ae
linxinqiang <linxinqiang@git.xrjiot.cn>
2020-06-16 16:28:21 +0800

    update

--- color_ranges.js
+++
@@ -1,4 +0,0 @@
-config.color_ranges = {
-  'label1': ['#3a6073', '#3a7bd5'],
-  'label2': ['#11998e', '#38ef7d'],
-};
--- imgs.js
+++
@@ -1,4 +0,0 @@
-var external_imgs = {
-    "中国": "img/中国.jpg",
-    "label2": "img/path/for/label2"
-}
